313bb20e317b760df1f44c2f4f05880aa11504f4
AkhunovAziz <akhunovaziz735@gmail.com>
2023-11-16 19:49:11 +0500

    fixing

--- ModelFirst/AppDataAccess/AppDbContext.cs
+++ ModelFirst/AppDataAccess/AppDbContext.cs
@@ -18,6 +18,7 @@ namespace ModelFirst.AppDataAccess
         {
             base.OnModelCreating(modelBuilder);
             modelBuilder.ApplyConfiguration(new StudentTypeConfigure());
+            modelBuilder.ApplyConfiguration(new ChildrenConfiguration());
         }
     }
 }
