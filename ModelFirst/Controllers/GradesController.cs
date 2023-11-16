f0662b82551dd16ad342b6ae116f68f562f8c7be
AkhunovAziz <akhunovaziz735@gmail.com>
2023-11-16 19:28:06 +0500

    All norm

---
+++ ModelFirst/Controllers/GradesController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ModelFirst.AppDataAccess;
+using ModelFirst.Models;
+
+namespace ModelFirst.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GradesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GradesController(AppDbContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async ValueTask<IEnumerable<Grade>> GetAll()
+        {
+            return await _context.Grades.ToListAsync();
+        }
+        [HttpPost]
+        public async ValueTask<bool> Create([FromForm]Grade grade)
+        {
+            try
+            {
+
+                await _context.Grades.AddAsync(grade);
+                await _context.SaveChangesAsync();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
